265a2f06b63ca22238be65f186b6ecfc2495288c
Radoslaw Smoronski <radoslaw.smoronski@gmail.com>
2024-03-19 18:58:53 +0100

    I added command 'help', simple command to show list of commands or details about commands.

--- MagazineManager/CmdDeveloperTool/CmdDeveloperTool.cs
+++ MagazineManager/CmdDeveloperTool/CmdDeveloperTool.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Runtime.InteropServices;
+using System.IO;
+using System.Xml.Linq;
 
 namespace MagazineManager.CmdDeveloperToolNS
 {
@@ -31,6 +33,7 @@ namespace MagazineManager.CmdDeveloperToolNS
 
                 switch (command)
                 {
+                    case "help": helpCommand(fullCommand); break;
                     case "addUser-s": CmdDeveloperToolUsers.addSimpleUser(fullCommand); break;
                     case "exit": exit(); break;
                     default: Console.WriteLine(""); break;
@@ -58,6 +61,62 @@ namespace MagazineManager.CmdDeveloperToolNS
 
             Console.WriteLine("   You didn't accept.");
             return false;
+        }
+
+        private void helpCommand(string fullCommand)
+        {
+            string[] commandParts = fullCommand.Split(' ');
+
+            string fileName = "helpCommand.xml";
+            string projectDirectory = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "..\\..\\..\\MagazineManager\\CmdDeveloperTool");
+            string filePath = Path.Combine(projectDirectory, fileName);
+
+            try
+            {
+                XDocument doc = XDocument.Load(filePath);
+
+                if(commandParts.Length == 1)
+                {
+                    Console.WriteLine("   [help] Commands lits:");
+
+                    foreach (XElement commandElement in doc.Descendants("command"))
+                    {
+
+                        string commandName = (string)commandElement.Attribute("name");
+
+                        string shortDescription = (string)commandElement.Element("short-description");
+
+                        Console.WriteLine($"   {commandName} - {shortDescription}");
+                    }
+
+                    Console.WriteLine(
+                        "\n   if you need more information about a given command,\n" +
+                        "   such as arguments, use the help <command name> command,\n" +
+                        "   e.g. help addUser-s");
+                }
+                else
+                {
+                    foreach (XElement commandElement in doc.Descendants("command"))
+                    {
+                        string commandName = (string)commandElement.Attribute("name");
+
+                        if (commandName == commandParts[1])
+                        {
+                            string longDescription = (string)commandElement.Element("long-description");
+
+                            Console.WriteLine($"   [help] {commandName} - {longDescription}");
+
+                            return;
+                        }
+                    }
+
+                    Console.WriteLine("   [help error]: This command doesn't exist.");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("   [help error]: " + ex.Message);
+            }
         }
 
     }
